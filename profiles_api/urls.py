5e1d855a4f33e941ed06d5a9cc3774e6be6a2aa8
capchitts <ltchitts@gmail.com>
2023-06-11 11:16:33 +0530

    Added create, retrieve, update, partial update and destroy in ViewSet

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import *
+
+
+router = DefaultRouter()
+router.register("hello-viewset",HelloViewSet,basename="hello-viewset")
+
 
 app_name = 'profiles_api'
 
 urlpatterns = [
-    path('hello-view/',HelloAPIView.as_view())
+    path('hello-view/',HelloAPIView.as_view()),
+    path('',include(router.urls))
 ]
